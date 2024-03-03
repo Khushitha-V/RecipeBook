a16c220c464358ccecf2b4098f76dd9053a37b67
Khushitha-V <khushithav12@gmail.com>
2024-03-03 17:16:33 +0530

    changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template,redirect,url_for,request
+from flask import Flask,render_template,redirect,url_for,request,flash
 app =Flask (__name__)
+app.secret_key="Khushi@129"
 recipes=[]
 @app.route("/")
 def home():
@@ -13,6 +14,21 @@ def add():
         data={"name": name, "ingredients": ingredients, "instructions": instructions}
         recipes.append(data)
     return render_template("addrecipe.html")
+@app.route("/display",methods=["POST","GET"])
+def dis():
+    flash("Recipe"+str(request.form(['nm']))+"is sucessfully added!")
+    return render_template("addrecipe.html")
+@app.route("/delete",methods=["DELETE"])
+def delete():
+    if request.method=="POST":
+        name = input("Enter recipe name to delete: ")
+    for recipe in recipes:
+        if recipe["name"] == name:
+            recipes.remove(recipe)
+            print(f"Recipe '{name}' deleted successfully!")
+            break
+    else:
+        print(f"Recipe '{name}' not found.")
 
 if __name__ == "__main__":
     app.run(debug=True)
